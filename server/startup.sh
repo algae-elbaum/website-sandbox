4276694dfeb74d018da2006ac633a1a9f1bde5cd
helbaum <helbaum@caltech.edu>
2018-05-12 21:25:58 -0700

    update startup options

--- server/startup.sh
+++ server/startup.sh
@@ -2,4 +2,7 @@
 
 # Run with sudo for port 80
 
-../venv/bin/python manage.py runmodwsgi --reload-on-changes --log-to-terminal --port 80 --user pi --group pi
+../venv/bin/python manage.py runmodwsgi --url-alias /media /home/pi/website-sandbox/server/media \
+                                        --user pi --group pi \
+                                        --reload-on-changes --log-to-terminal --startup-log \
+                                        --port 80
