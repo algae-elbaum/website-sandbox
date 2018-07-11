c3ed442a3c7b520c3417ed66a8906711cf907201
helbaum <helbaum@caltech.edu>
2018-07-10 19:14:07 -0700

    Polish populate_db command

--- server/muse/management/commands/populate_db.py
+++ server/muse/management/commands/populate_db.py
@@ -4,30 +4,46 @@ from django.core.files import File
 from django.conf import settings
 from muse.models import Album, Song
 from os import walk, path
-from muse.utl import is_music
+import muse.utl as utl
+
+default_album_name = "loose songs"
 
 class Command(BaseCommand):
-    args = '<foo bar ...>'
+    args = '<music_root_dir>'
     help = 'our help string comes here'
 
-    music_root = path.join(settings.MEDIA_ROOT, 'music')
+    def add_arguments(self, parser):
+        parser.add_argument('music_root', nargs=1, type=str)
 
-    def _populate_album_table(self):
-        root = Album(name=path.basename(self.music_root))
-        root.save()
-        for root, dirs, files in walk(self.music_root):
+    def _populate_album_table(self, music_root):
+        default_album = Album(name=default_album_name)
+        default_album.save()
+        for root, dirs, files in walk(music_root):
             for d in dirs:
-                print(d)
+                # Don't double add if rerunning command
                 if not Album.objects.filter(name=d):
+                    print(d)
                     new = Album(name=d)
                     new.save()
+            # Only immediate subdirs of music_root are albums, so break
+            break
 
-    def _populate_song_table(self):
-        for root, dirs, files in walk(self.music_root):
+    def _populate_song_table(self, music_root):
+        for root, dirs, files in walk(music_root):
+            # Only look for songs in music_root and immediate subdirs
+            if root != music_root and path.dirname(root) != music_root:
+                continue
+            print(root)
             for fname in files:
-                if is_music(fname):
-                    album = Album.objects.get(name=path.basename(root))
-                    full_path = path.join(root, fname)
+                if not utl.is_music(fname):
+                    continue
+                if (root == music_root):
+                    album_name = default_album_name
+                else:
+                    album_name = path.basename(root)
+                album = Album.objects.get(name=album_name)
+                full_path = path.join(root, fname)
+                if not Song.objects.filter(name=fname):
                     print(album.name, full_path)
                     with open(full_path, 'rb') as f:
                         new = Song(name=fname, album=album, music_file=File(f))
@@ -35,5 +51,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         Album.objects.all().delete()
-        self._populate_album_table()
-        self._populate_song_table()
+        self._populate_album_table(options['music_root'][0])
+        print('----')
+        self._populate_song_table(options['music_root'][0])
--- server/muse/utl.py
+++ server/muse/utl.py
@@ -1,7 +1,9 @@
 from .settings import MUSIC_FILETYPES
 
 def is_music(fname):
+    """Return whether a file is a music file based on its extension"""
     for ft in MUSIC_FILETYPES:
         if fname.endswith(ft):
             return True
     return False
+
