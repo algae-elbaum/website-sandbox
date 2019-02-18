eff3a2fd9f2d6b774fc34f36365a8e5b529075e0
helbaum <helbaum@caltech.edu>
2019-02-18 14:32:40 -0800

    add (x) to songs in queue, plus a load of refactoring

--- server/static/muse/player.js
+++ server/static/muse/player.js
@@ -1,12 +1,189 @@
-var playlist = [];
-var curr_index = -1; // The current playlist element
-var max_prev = 15; // Display up to this many songs in the queue in front of the currently playing song
+var dflt_pl =
+{
+    playlist: [],
+    curr_idx: -1, // The current song idx 
+    max_prev: 15, // Num songs to show in the queue before the current song
+
+    // Queue an album into the playlist
+    queue_album: function (album_id)
+    {
+        var req = new XMLHttpRequest();
+        req.open('GET', 'api/albums/' + album_id, true);
+        req.timeout = 1000;
+        req.onload = 
+            function (loadEvent) 
+            {
+                if (req.readyState == 4 && req.status == 200)
+                {
+                    var resp = JSON.parse(req.responseText);                 
+                    resp.songs.sort(function(o1, o2){return o1.name.localeCompare(o2.name);});
+                    this.playlist = this.playlist.concat(resp.songs);
+                    this.refresh_playlist();
+                }
+            }.bind(this);
+        req.send();
+    },
+
+    // Add album to playlist and play. Album is inserted after the current song
+    play_album: function (album_id)
+    {
+        var req = new XMLHttpRequest();
+        req.open('GET', 'api/albums/' + album_id, true);
+        req.timeout = 1000;
+        req.onload = 
+            function (loadEvent) 
+            {
+                if (req.readyState == 4 && req.status == 200)
+                {
+                    var resp = JSON.parse(req.responseText);                 
+                    resp.songs.sort(function(o1, o2){return o1.name.localeCompare(o2.name);});
+                    var prev_songs = this.playlist.slice(0, this.curr_idx + 1);
+                    var next_songs = this.playlist.slice(this.curr_idx + 1);
+                    this.playlist = prev_songs.concat(resp.songs).concat(next_songs);
+                    this.refresh_playlist();
+                    this.play_next();
+                }
+            }.bind(this);
+        req.send();
+    },
+
+    // Queue song to end of playlist
+    queue_song: function (song_id, song_name)
+    {
+        var song_json = {'name': song_name, 'id': song_id};
+        this.playlist.push(song_json);
+        this.refresh_playlist();
+    },
+
+    // Add song to playlist and play. Song is inserted after the current song
+    play_song: function (song_id, song_name)
+    {
+        var song_json = {'name': song_name, 'id': song_id};
+        this.playlist.splice(this.curr_idx+1, 0, song_json);
+        this.play_next();
+    },
+    
+    remove_song: function (idx)
+    {
+        this.playlist.splice(idx, 1);
+        this.refresh_playlist();
+        console.log(idx);
+        console.log(this.curr_idx);
+        if (Number(idx) === this.curr_idx)
+            this.jump_by(0);
+    },
+
+    // Update what slice of the playlist is displayed on screen, and update the 
+    // current song pointer
+    refresh_playlist: function ()
+    {
+        // Easy and fast enough to just remake the <li>
+        $('#playlist').empty();
+        var pl_div = document.getElementById('playlist');
+        var playlist_start = Math.max(0, this.curr_idx - this.max_prev);
+        for (var i = playlist_start;  i < this.playlist.length; i++)
+        {
+            var new_li = document.createElement('li');
+            var rem = document.createElement('a');
+            var song = document.createElement('a');
+
+            // Remove song button
+            rem.innerText = "(x)";
+            rem.onclick =
+                function (event)
+                {
+                    this.remove_song(event.srcElement.getAttribute('data-idx'));
+                }.bind(this);
+
+            rem.setAttribute('data-idx', i);
+            new_li.appendChild(rem);
+
+            // Song link
+            song.innerText = this.playlist[i].name;
+            song.onclick =
+                function (event)
+                {
+                    this.curr_idx = event.srcElement.getAttribute('data-idx') - 1;
+                    this.play_next();
+                }.bind(this);
+                    
+            song.setAttribute('data-idx', i);
+
+            if (i === this.curr_idx)
+                song.innerText += '    <-------- Currently Playing';
+            new_li.appendChild(song);
+
+            pl_div.appendChild(new_li);
+        }
+    },
+
+    clear_playlist: function (event)
+    {
+        this.playlist = [];
+        this.curr_idx = -1;
+        this.refresh_playlist();
+    },
+
+
+    // Change current position in the playlist by i
+    jump_by: function (i)
+    {
+        // Allow being out of bounds by one index, in which case nothing plays
+        this.curr_idx = Math.min(this.playlist.length,
+                                 Math.max(-1, this.curr_idx + i));
+        if (this.curr_idx === -1 || this.curr_idx === this.playlist.length)
+            document.getElementById('curr_song').src = '';
+        else
+            this.load_song(this.playlist[this.curr_idx].id);
+        this.refresh_playlist();
+    },
+
+    play_next: function ()
+    {
+        this.jump_by(1);
+    },
+
+    play_prev: function ()
+    {
+        this.jump_by(-1);
+    },
+
+    // Actually play a song!
+    load_song: function (song_id)
+    {
+        var req = new XMLHttpRequest();
+        req.open('GET', 'api/songs/' + song_id, true);
+        req.timeout = 1000;
+        req.onload = 
+            function (loadEvent) 
+            {
+                if (req.readyState == 4 && req.status == 200)
+                {
+                    var resp = JSON.parse(req.responseText);                 
+                    var audio = document.getElementById('curr_song');
+                    audio.src = resp.file_url;
+                }
+            }
+        req.send();
+    }
+};
+
 
 function page_load()
 {
     get_albums();
 }
 
+function call_with_evt_data(fun)
+{
+    return function(event)
+    {
+        fun(event.srcElement.getAttribute('data-id'),
+            event.srcElement.getAttribute('data-name'));
+    }
+}
+
+// Fetch and display list of stored albums
 function get_albums()
 {
     var req = new XMLHttpRequest();
@@ -26,6 +203,7 @@ function get_albums()
 
 }
 
+// Add an album to the displayed album list
 function add_album(name, id)
 {
     // cba to get jquery to get onlick right
@@ -37,12 +215,12 @@ function add_album(name, id)
 
     // Queue button
     queue_btn.innerText = "queue";
-    queue_btn.onclick = queue_album;
+    queue_btn.onclick = call_with_evt_data(dflt_pl.queue_album.bind(dflt_pl));
     queue_btn.setAttribute('data-id', id);
 
     // Play button
     play_btn.innerText = "play";
-    play_btn.onclick = play_album;
+    play_btn.onclick = call_with_evt_data(dflt_pl.play_album.bind(dflt_pl));
     play_btn.setAttribute('data-id', id);
 
     // Album link
@@ -56,6 +234,41 @@ function add_album(name, id)
     albums_div.appendChild(new_li);
 }
 
+// Add a song to the displayed song list
+function add_song(name, id)
+{
+    // cba to get jquery to get onlick right
+    var album_div = document.getElementById("curr_album");
+    var new_li = document.createElement("li");
+    var queue_btn = document.createElement("button");
+    var play_btn = document.createElement("button");
+    var text_a = document.createElement("a");
+
+    // Queue button
+    queue_btn.innerText = "queue";
+    queue_btn.onclick = call_with_evt_data(dflt_pl.queue_song.bind(dflt_pl));
+    queue_btn.setAttribute('data-id', id);
+    queue_btn.setAttribute('data-name', name);
+
+    // Play button
+    play_btn.innerText = "play";
+    play_btn.onclick = call_with_evt_data(dflt_pl.play_song.bind(dflt_pl));
+    play_btn.setAttribute('data-id', id);
+    play_btn.setAttribute('data-name', name);
+
+    // Song link
+    text_a.innerText = name;
+    text_a.onclick = call_with_evt_data(dflt_pl.play_song.bind(dflt_pl));
+    text_a.setAttribute('data-id', id);
+    text_a.setAttribute('data-name', name);
+
+    new_li.appendChild(queue_btn);
+    new_li.appendChild(play_btn);
+    new_li.appendChild(text_a);
+    album_div.appendChild(new_li);
+}
+
+// List an album's songs in the displayed song list
 function show_album(event)
 {
     var req = new XMLHttpRequest();
@@ -77,169 +290,4 @@ function show_album(event)
     req.send();
 }
 
-function queue_album(event)
-{
-    var req = new XMLHttpRequest();
-    req.open('GET', 'api/albums/' + event.srcElement.getAttribute('data-id'), true);
-    req.timeout = 1000;
-    req.onload = 
-        function (loadEvent) 
-        {
-            if (req.readyState == 4 && req.status == 200)
-            {
-                var resp = JSON.parse(req.responseText);                 
-                resp.songs.sort(function(o1, o2){return o1.name.localeCompare(o2.name);});
-                playlist = playlist.concat(resp.songs);
-                refresh_playlist();
-            }
-        }
-    req.send();
-}
 
-function play_album(event)
-{
-    var req = new XMLHttpRequest();
-    req.open('GET', 'api/albums/' + event.srcElement.getAttribute('data-id'), true);
-    req.timeout = 1000;
-    req.onload = 
-        function (loadEvent) 
-        {
-            if (req.readyState == 4 && req.status == 200)
-            {
-                var resp = JSON.parse(req.responseText);                 
-                resp.songs.sort(function(o1, o2){return o1.name.localeCompare(o2.name);});
-                var prev_songs = playlist.slice(0, curr_index + 1);
-                var next_songs = playlist.slice(curr_index + 1);
-                playlist = prev_songs.concat(resp.songs).concat(next_songs);
-                refresh_playlist();
-                play_next();
-            }
-        }
-    req.send();
-}
-
-function add_song(name, id)
-{
-    // cba to get jquery to get onlick right
-    var album_div = document.getElementById("curr_album");
-    var new_li = document.createElement("li");
-    var queue_btn = document.createElement("button");
-    var play_btn = document.createElement("button");
-    var text_a = document.createElement("a");
-
-    // Queue button
-    queue_btn.innerText = "queue";
-    queue_btn.onclick = queue_song;
-    queue_btn.setAttribute('data-id', id);
-    queue_btn.setAttribute('data-name', name);
-
-    // Play button
-    play_btn.innerText = "play";
-    play_btn.onclick = play_song;
-    play_btn.setAttribute('data-id', id);
-    play_btn.setAttribute('data-name', name);
-
-    // Song link
-    text_a.innerText = name;
-    text_a.onclick = play_song;
-    text_a.setAttribute('data-id', id);
-    text_a.setAttribute('data-name', name);
-
-    new_li.appendChild(queue_btn);
-    new_li.appendChild(play_btn);
-    new_li.appendChild(text_a);
-    album_div.appendChild(new_li);
-}
-
-function queue_song(event)
-{
-    var song_json = {'name': event.srcElement.getAttribute('data-name'),
-                     'id': event.srcElement.getAttribute('data-id')};
-    playlist.push(song_json);
-    refresh_playlist();
-}
-
-function play_song(event)
-{
-    var song_json = {'name': event.srcElement.getAttribute('data-name'),
-                     'id': event.srcElement.getAttribute('data-id')};
-    playlist.splice(curr_index+1, 0, song_json);
-    refresh_playlist();
-    play_next();
-}
-
-// Update what slice of the playlist is displayed on screen, and update the 
-// current song pointer
-function refresh_playlist()
-{
-    // Easy and fast enough to just remake the <li>
-    $('#playlist').empty();
-    var pl_div = document.getElementById('playlist');
-    for (var i = Math.max(0, curr_index - max_prev); i < playlist.length; i++)
-    {
-        var new_li = document.createElement('li');
-        var song = document.createElement('a');
-        song.innerText = playlist[i].name;
-        song.onclick =
-            function (event)
-            {
-                curr_index = event.srcElement.getAttribute('data-idx') - 1;
-                play_next();
-            };
-                
-        song.setAttribute('data-idx', i);
-        song.setAttribute('data-id', playlist[i].id);
-
-        if (i === curr_index)
-            song.innerText += '    <-------- Currently Playing';
-        new_li.appendChild(song);
-        pl_div.appendChild(new_li);
-    }
-}
-function clear_playlist(event)
-{
-    playlist = [];
-    curr_index = -1;
-    refresh_playlist();
-}
-
-
-// Change current position in the playlist by i
-function jump_by(i)
-{
-    // Allow being out of bounds by one index, in which case nothing plays
-    curr_index = Math.min(playlist.length, Math.max(-1, curr_index + i));
-    if (curr_index === -1 || curr_index === playlist.length)
-        document.getElementById('curr_song').src = '';
-    else
-        load_song(playlist[curr_index].id);
-    refresh_playlist();
-}
-
-function play_next()
-{
-    jump_by(1);
-}
-
-function play_prev()
-{
-    jump_by(-1);
-}
-
-function load_song(song_id)
-{
-    var req = new XMLHttpRequest();
-    req.open('GET', 'api/songs/' + song_id, true);
-    req.timeout = 1000;
-    req.onload = 
-        function (loadEvent) 
-        {
-            if (req.readyState == 4 && req.status == 200)
-            {
-                var resp = JSON.parse(req.responseText);                 
-                var audio = document.getElementById('curr_song');
-                audio.src = resp.file_url;
-            }
-        }
-    req.send();
-}
